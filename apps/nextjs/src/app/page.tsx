1f6cb77e32b48bdb7e5f2c940a2cd9b204e7a8a7
Caleb Rivera <calebrossr@gmail.com>
2024-10-16 02:30:26 -0400

    add basic Discord oauth example

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,10 +1,27 @@
+import { deleteSessionTokenCookie, getCurrentSession } from "@acme/auth/next";
 import { Button } from "@acme/ui/button";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const { user } = await getCurrentSession();
   return (
-    <div>
-      Hello World!
-      <Button>Sign Up with Discord</Button>
-    </div>
+    <main>
+      {user === null ? (
+        <a href="/login/discord">Sign Up with Discord</a>
+      ) : (
+        <form>
+          <Button
+            // eslint-disable-next-line @typescript-eslint/require-await
+            formAction={async () => {
+              "use server";
+
+              deleteSessionTokenCookie();
+            }}
+          >
+            Sign Out
+          </Button>
+        </form>
+      )}
+      <div>{JSON.stringify(user, null, 2)}</div>
+    </main>
   );
 }
