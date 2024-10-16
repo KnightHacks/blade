c93f92958533f5b097054de19fe5fa93021b9c83
Caleb Rivera <calebrossr@gmail.com>
2024-10-16 02:30:11 -0400

    add function to get current user session

--- packages/auth/src/next.ts
+++ packages/auth/src/next.ts
@@ -1,9 +1,13 @@
+import { cache } from "react";
 import { cookies } from "next/headers";
+
+import type { SessionValidationResult } from ".";
+import { validateSession } from ".";
 
 export function setSessionTokenCookie(token: string, expires: Date): void {
   cookies().set("session", token, {
     httpOnly: true,
-    // eslint-disable-next-line no-restricted-properties
+
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
     expires,
@@ -14,10 +18,21 @@ export function setSessionTokenCookie(token: string, expires: Date): void {
 export function deleteSessionTokenCookie(): void {
   cookies().set("session", "", {
     httpOnly: true,
-    // eslint-disable-next-line no-restricted-properties
+
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
     expires: 0,
     path: "/",
   });
 }
+
+export const getCurrentSession = cache(
+  async (): Promise<SessionValidationResult> => {
+    const token = cookies().get("session")?.value ?? null;
+    if (token === null) {
+      return { session: null, user: null };
+    }
+    const result = await validateSession(token);
+    return result;
+  },
+);
