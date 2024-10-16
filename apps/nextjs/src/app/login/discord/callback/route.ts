c52429c579d7afec48c749c84dbfcb403c6a3248
Caleb Rivera <calebrossr@gmail.com>
2024-10-16 02:56:44 -0400

    upgrade Arctic to v2

--- apps/nextjs/src/app/login/discord/callback/route.ts
+++ apps/nextjs/src/app/login/discord/callback/route.ts
@@ -1,5 +1,7 @@
-import type { DiscordTokens } from "arctic";
+import type { OAuth2Tokens } from "arctic";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { ArcticFetchError, OAuth2RequestError } from "arctic";
 
 import { createSession, generateSessionToken } from "@acme/auth";
 import { setSessionTokenCookie } from "@acme/auth/next";
@@ -13,78 +15,84 @@ export async function GET(request: Request): Promise<Response> {
   const state = url.searchParams.get("state");
   const storedState = cookies().get("discord_oauth_state")?.value ?? null;
   if (code === null || state === null || storedState === null) {
-    return new Response(null, {
-      status: 400,
-    });
+    return NextResponse.json(
+      { error: "Invalid request" },
+      {
+        status: 400,
+      },
+    );
   }
   if (state !== storedState) {
-    return new Response(null, {
-      status: 400,
-    });
-  }
-
-  let tokens: DiscordTokens;
-  try {
-    tokens = await discord.validateAuthorizationCode(code);
-  } catch {
-    // Invalid code or client credentials
-    return new Response(null, {
-      status: 400,
-    });
-  }
-
-  const discordUserResponse = await fetch("https://discord.com/api/users/@me", {
-    headers: {
-      Authorization: `Bearer ${tokens.accessToken}`,
-    },
-  });
-  const discordUser = (await discordUserResponse.json()) as {
-    id: string;
-    username: string;
-  };
-
-  const discordUserId = discordUser.id;
-  const discordUsername = discordUser.username;
-
-  const existingUser = await db.query.UserTable.findFirst({
-    where: (user, t) => t.eq(user.discordId, discordUserId),
-  });
-
-  if (existingUser) {
-    const sessionToken = generateSessionToken();
-    const session = await createSession(sessionToken, existingUser.id);
-    setSessionTokenCookie(sessionToken, session.expiresAt);
-    return new Response(null, {
-      status: 302,
-      headers: {
-        Location: "/",
+    return NextResponse.json(
+      { error: "Invalid state" },
+      {
+        status: 400,
       },
-    });
+    );
   }
 
-  const [user] = await db
-    .insert(UserTable)
-    .values({
-      discordId: discordUserId,
-      discordUsername: discordUsername,
-    })
-    .returning({
-      id: UserTable.id,
+  try {
+    const tokens: OAuth2Tokens = await discord.validateAuthorizationCode(code);
+    const accessToken = tokens.accessToken();
+
+    const discordUserResponse = await fetch(
+      "https://discord.com/api/users/@me",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    const discordUser = (await discordUserResponse.json()) as {
+      id: string;
+      username: string;
+    };
+
+    const discordUserId = discordUser.id;
+    const discordUsername = discordUser.username;
+
+    const existingUser = await db.query.UserTable.findFirst({
+      where: (user, t) => t.eq(user.discordId, discordUserId),
     });
 
-  if (!user) {
-    return new Response(null, {
-      status: 500,
-    });
+    if (existingUser) {
+      const sessionToken = generateSessionToken();
+      const session = await createSession(sessionToken, existingUser.id);
+      setSessionTokenCookie(sessionToken, session.expiresAt);
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    const [user] = await db
+      .insert(UserTable)
+      .values({
+        discordId: discordUserId,
+        discordUsername: discordUsername,
+      })
+      .returning({
+        id: UserTable.id,
+      });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Failed to create user" },
+        {
+          status: 500,
+        },
+      );
+    }
+
+    const sessionToken = generateSessionToken();
+    const session = await createSession(sessionToken, user.id);
+    setSessionTokenCookie(sessionToken, session.expiresAt);
+  } catch (e) {
+    if (e instanceof OAuth2RequestError) {
+      return NextResponse.json({ error: e.message });
+    }
+    if (e instanceof ArcticFetchError) {
+      return NextResponse.json({ error: e.message });
+    }
   }
 
-  const sessionToken = generateSessionToken();
-  const session = await createSession(sessionToken, user.id);
-  setSessionTokenCookie(sessionToken, session.expiresAt);
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: "/",
-    },
-  });
+  return NextResponse.redirect(new URL("/", request.url));
 }
--- apps/nextjs/src/app/login/discord/route.ts
+++ apps/nextjs/src/app/login/discord/route.ts
@@ -1,20 +1,18 @@
-/* eslint-disable no-restricted-properties */
-
 import { cookies } from "next/headers";
 import { generateState } from "arctic";
 
 import { discord } from "@acme/auth/oauth";
 
-export async function GET() {
+import { env } from "~/env";
+
+export function GET() {
   const state = generateState();
-  const url = await discord.createAuthorizationURL(state, {
-    scopes: ["identify"],
-  });
+  const url = discord.createAuthorizationURL(state, ["identify"]);
 
   cookies().set("discord_oauth_state", state, {
     path: "/",
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: env.NODE_ENV === "production",
     maxAge: 60 * 10, // 10 minutes
     sameSite: "lax",
   });
