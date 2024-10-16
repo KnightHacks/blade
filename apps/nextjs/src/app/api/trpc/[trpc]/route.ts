405439258597416edf717fc10a9a9debafa0b7fa
Caleb Rivera <calebrossr@gmail.com>
2024-10-16 03:22:58 -0400

    disable eslint rule for using console

--- apps/nextjs/src/app/api/trpc/[trpc]/route.ts
+++ apps/nextjs/src/app/api/trpc/[trpc]/route.ts
@@ -32,6 +32,7 @@ const handler = async (req: NextRequest) => {
         headers: req.headers,
       }),
     onError({ error, path }) {
+      // eslint-disable-next-line no-console -- we want to log errors
       console.error(`>>> tRPC Error on '${path}'`, error);
     },
   });
