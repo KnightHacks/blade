fa92e18141cc4852ec17319db14a71ad48e7c087
Caleb Rivera <calebrossr@gmail.com>
2024-10-18 12:38:25 -0400

    add db export from db

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,4 +1,5 @@
 import type { AnyD1Database } from "drizzle-orm/d1";
+import { getRequestContext } from "@cloudflare/next-on-pages";
 import { drizzle } from "drizzle-orm/d1";
 
 import * as schema from "./schema";
@@ -6,3 +7,10 @@ import * as schema from "./schema";
 export function createD1DrizzleClient(db: AnyD1Database) {
   return drizzle(db, { schema });
 }
+
+export const db = (() => {
+  const {
+    env: { DB },
+  } = getRequestContext();
+  return createD1DrizzleClient(DB);
+})();
