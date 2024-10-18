c66855a8b2e42b3efe10469a2e291cfc362e7d35
Caleb Rivera <calebrossr@gmail.com>
2024-10-18 11:19:17 -0400

    simplify home page

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,27 +1,7 @@
-import { deleteSessionTokenCookie, getCurrentSession } from "@blade/auth/next";
-import { Button } from "@blade/ui/button";
-
-export default async function HomePage() {
-  const { user } = await getCurrentSession();
+export default function HomePage() {
   return (
     <main>
-      {user === null ? (
-        <a href="/login/discord">Sign Up with Discord</a>
-      ) : (
-        <form>
-          <Button
-            // eslint-disable-next-line @typescript-eslint/require-await
-            formAction={async () => {
-              "use server";
-
-              deleteSessionTokenCookie();
-            }}
-          >
-            Sign Out
-          </Button>
-        </form>
-      )}
-      <div>{JSON.stringify(user, null, 2)}</div>
+      <div>Hello, World!</div>
     </main>
   );
 }
