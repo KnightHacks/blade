ddcf5cd24bd5b890aa7cb2085668c197ac395596
Caleb Rivera <calebrossr@gmail.com>
2024-10-18 11:17:27 -0400

    move to sqlite

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,40 +1,22 @@
 import type { InferSelectModel } from "drizzle-orm";
-import { integer, pgTable } from "drizzle-orm/pg-core";
+import { integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
-// export const Post = pgTable("post", (t) => ({
-//   id: t.uuid().notNull().primaryKey().defaultRandom(),
-//   title: t.varchar({ length: 256 }).notNull(),
-//   content: t.text().notNull(),
-//   createdAt: t.timestamp().defaultNow().notNull(),
-//   updatedAt: t
-//     .timestamp({ mode: "date", withTimezone: true })
-//     .$onUpdateFn(() => sql`now()`),
-// }));
-
-// export const CreatePostSchema = createInsertSchema(Post, {
-//   title: z.string().max(256),
-//   content: z.string().max(256),
-// }).omit({
-//   id: true,
-//   createdAt: true,
-//   updatedAt: true,
-// });
-
-export const UserTable = pgTable("user", (t) => ({
-  id: t.serial().primaryKey(),
-  discordId: t.varchar({ length: 32 }).notNull().unique(),
-  discordUsername: t.varchar({ length: 32 }).notNull(),
+export const UserTable = sqliteTable("user", (t) => ({
+  id: t.integer().primaryKey({
+    autoIncrement: true,
+  }),
+  discordId: t.text({ length: 32 }).notNull().unique(),
+  discordUsername: t.text({ length: 32 }).notNull(),
 }));
 
-export const SessionTable = pgTable("session", (t) => ({
+export const SessionTable = sqliteTable("session", (t) => ({
   id: t.text().primaryKey(),
   userId: integer()
     .notNull()
     .references(() => UserTable.id),
   expiresAt: t
-    .timestamp({
-      withTimezone: true,
-      mode: "date",
+    .integer({
+      mode: "timestamp",
     })
     .notNull(),
 }));
