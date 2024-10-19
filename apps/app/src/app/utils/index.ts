7fc746d87a8b9bf4f98c5a7b6cc44c8a6ffd96b2
Caleb Rivera <calebrossr@gmail.com>
2024-10-19 17:56:17 -0400

    add cookie utils

--- apps/app/src/app/utils/index.ts
+++ apps/app/src/app/utils/index.ts
@@ -2,15 +2,11 @@ import type { SessionValidationResult } from "@blade/auth";
 import { cache } from "react";
 import { cookies } from "next/headers";
 import { validateSessionToken } from "@blade/auth";
-import { createD1DrizzleClient } from "@blade/db/client";
-import { getRequestContext } from "@cloudflare/next-on-pages";
+import { db } from "@blade/db/client";
+import { env } from "env";
 
 export const getCurrentSession = cache(
   async (): Promise<SessionValidationResult> => {
-    const {
-      env: { DB },
-    } = getRequestContext();
-    const db = createD1DrizzleClient(DB);
     const token = cookies().get("session")?.value ?? null;
     if (token === null) {
       return { session: null, user: null };
@@ -19,3 +15,23 @@ export const getCurrentSession = cache(
     return result;
   },
 );
+
+export function setSessionTokenCookie(token: string, expiresAt: Date): void {
+  cookies().set("session", token, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: env.NODE_ENV === "production",
+    expires: expiresAt,
+    path: "/",
+  });
+}
+
+export function deleteSessionTokenCookie(): void {
+  cookies().set("session", "", {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: env.NODE_ENV === "production",
+    maxAge: 0,
+    path: "/",
+  });
+}
