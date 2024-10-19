081a21c6264df71198eaf0e9fa55e74967fb39e2
Caleb Rivera <calebrossr@gmail.com>
2024-10-19 17:57:37 -0400

    further showcase auth and db on home page

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,16 +1,31 @@
 import { db } from "@blade/db/client";
+import { Button } from "@blade/ui/button";
+
+import { deleteSessionTokenCookie, getCurrentSession } from "./utils";
 
 export const runtime = "edge";
 
 export default async function HomePage() {
+  const { session } = await getCurrentSession();
   const users = await db.query.UserTable.findMany();
   return (
     <main>
-      <div>
+      {session === null ? (
         <a href="/login/discord">Login with Discord</a>
-      </div>
-
-      {JSON.stringify(users)}
+      ) : (
+        <form>
+          <Button
+            // eslint-disable-next-line @typescript-eslint/require-await -- form actions must be async
+            formAction={async () => {
+              "use server";
+              deleteSessionTokenCookie();
+            }}
+          >
+            Logout
+          </Button>
+        </form>
+      )}
+      <div>{JSON.stringify(users)}</div>
     </main>
   );
 }
