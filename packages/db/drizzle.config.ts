4e8abe01c45256a0f27a76a1c7dee4e42b9d7b56
Caleb Rivera <calebrossr@gmail.com>
2024-10-19 05:40:26 -0400

    use local sqlite url with libsql

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -4,11 +4,8 @@ export default {
   schema: "./src/schema.ts",
   out: "./src/migrations",
   dialect: "sqlite",
-  driver: "d1-http",
   dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    token: process.env.CLOUDFLARE_D1_TOKEN!,
+    url: process.env.LOCAL_DATABASE_URL!,
   },
   casing: "snake_case",
 } satisfies Config;
