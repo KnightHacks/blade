bcd9e135871a242328750fb976588bbdde65f6e0
Caleb Rivera <calebrossr@gmail.com>
2024-10-19 07:23:36 -0400

    refactor db client to handle different environments

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -9,8 +9,13 @@ export function createD1DrizzleClient(db: AnyD1Database) {
 }
 
 export const db = (() => {
-  const {
-    env: { DB },
-  } = getRequestContext();
-  return createD1DrizzleClient(DB);
+  if (process.env.NODE_ENV === "development") {
+    const {
+      env: { DB },
+    } = getRequestContext();
+    return createD1DrizzleClient(DB);
+  }
+
+  // eslint-disable-next-line turbo/no-undeclared-env-vars -- DB will attached to process.env by the Cloudflare Worker runtime
+  return createD1DrizzleClient(process.env.DB as unknown as D1Database);
 })();
