d6c417480ff36380dee019041c3c32f3fc706708
Caleb Rivera <calebrossr@gmail.com>
2024-10-21 00:29:06 -0400

    refactor db set up

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,21 +1,14 @@
-import type { AnyD1Database } from "drizzle-orm/d1";
 import { getRequestContext } from "@cloudflare/next-on-pages";
 import { drizzle } from "drizzle-orm/d1";
 
 import * as schema from "./schema";
 
-export function createD1DrizzleClient(db: AnyD1Database) {
-  return drizzle(db, { schema });
+export function createDbClient() {
+  const {
+    env: { DB },
+  } = getRequestContext();
+
+  return drizzle(DB, { schema });
 }
 
-export const db = (() => {
-  if (process.env.NODE_ENV === "development") {
-    const {
-      env: { DB },
-    } = getRequestContext();
-    return createD1DrizzleClient(DB);
-  }
-
-  // eslint-disable-next-line turbo/no-undeclared-env-vars -- DB will attached to process.env by the Cloudflare Worker runtime
-  return createD1DrizzleClient(process.env.DB as unknown as D1Database);
-})();
+export const db = createDbClient();
