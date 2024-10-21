7fc30dfd7a265a0a3485ad41db620d8b80487ff4
Caleb Rivera <calebrossr@gmail.com>
2024-10-21 00:50:52 -0400

    refactor db client to call process.env in prod

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -4,11 +4,15 @@ import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./schema";
 
 export function createDbClient() {
-  const {
-    env: { DB },
-  } = getRequestContext();
+  if (process.env.NODE_ENV === "development") {
+    const {
+      env: { DB },
+    } = getRequestContext();
+    return drizzle(DB, { schema });
+  }
 
-  return drizzle(DB, { schema });
+  // eslint-disable-next-line turbo/no-undeclared-env-vars -- DB will be attached to `process.env` by the Cloudflare runtime
+  return drizzle(process.env.DB as unknown as D1Database, { schema });
 }
 
 export const db = createDbClient();
